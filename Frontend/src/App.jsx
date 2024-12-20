a4ff2a9e00db040f2887436799ab5181b8bb12e9
gourab Das <gourabdas10122000@gmail.com>
2024-12-20 16:59:45 +0530

    adding a demo page for testing vercel link

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route , Routes } from "react-router-dom";
-import { ImageUploading } from "./pages/ImageUploading/ImageUploading";
-
+import { ImageUploading } from "./pages/ImageUploading/ImageUploading.jsx";
+import { DemoPage } from "./pages/DemoPage/DemoPage.jsx";
 function App() {
   return (
     <>
     <Routes>
+     <Route path="/" element={<DemoPage/>}/>
      <Route path="/image"  element={<ImageUploading/>}/>
     </Routes>
     </>
